dc2315b0652a0115cb5b39e84fabe02dc5e06822
allexis <allexissoares@gmail.com>
2022-09-13 20:24:03 -0300

    feat: add TabbarViewController mounted in the screen

--- twitter/Controllers/Core/HomeViewController.swift
+++ twitter/Controllers/Core/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  twitter
 //
 //  Created by allexis figueiredo on 13/09/22.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-

810129724aae229ec059ffe8a72d3b98b2f5ee4a
allexis <allexissoares@gmail.com>
2022-09-13 19:51:49 -0300

    Initial Commit

---
+++ twitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twitter
+//
+//  Created by allexis figueiredo on 13/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

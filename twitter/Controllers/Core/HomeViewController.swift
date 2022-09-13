f97fa33257a0f22d2ae3b3f6f4b900a431e86dc2
allexis <allexissoares@gmail.com>
2022-09-13 20:59:39 -0300

    feat: add initial tableView to HomeViewController

--- twitter/Controllers/Core/HomeViewController.swift
+++ twitter/Controllers/Core/HomeViewController.swift
@@ -8,11 +8,40 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private let timelineTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.addSubview(timelineTableView)
+        timelineTableView.delegate = self
+        timelineTableView.dataSource = self
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        timelineTableView.frame = view.frame
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "Tweet"
+        cell.contentConfiguration = content
+        
+        return cell
+    }
 }
